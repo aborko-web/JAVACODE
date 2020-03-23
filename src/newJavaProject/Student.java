35a57d67c7feba94da0b9077adf91d59bdac23a2
aborko-web <andualemmarkos@gmail.com>
2020-03-23 12:00:49 -0400

    new java project

---
+++ src/newJavaProject/Student.java
@@ -0,0 +1,50 @@
+package newJavaProject;
+
+public class Student {
+	       
+	
+    int rollno;
+	String name;
+	float fee;
+	
+	Student(int rollno,String name,float fee){
+	this.rollno=rollno;
+	this.name=name;
+	this.fee=fee;
+	}
+	
+	void display(){
+		System.out.println(rollno+" "+name+" "+fee);
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//defining fields  
+	
+		 int id=8989;//field or data member or instance variable  
+		 //String name="ALEX MACHARESs";  //variable 
+		 
+		 
+		
+			
+		 //creating main method inside the Student class  
+		 public static void main(String args[]){  
+			 			//Creating an object or instance  
+			 		//	Student s1=new Student();//creating an object of Student  
+		  //Printing values of the object  
+		//  System.out.println(s1.id);//accessing member through reference variable  
+		//  System.out.println(s1.name); 
+		  
+		  
+		 
+
+}
+
+	
+		 }
---
+++ src/newJavaProject/bike1.java
@@ -0,0 +1,17 @@
+package newJavaProject;
+
+public class bike1 {
+	//creating a default constructor  
+	
+		bike1(){
+			System.out.println("Bike is created");
+		
+			} 
+		
+		//main method  
+		public static void main(String args[]){  
+		//calling a default constructor  
+		bike1 b=new bike1(); 
+	
+}
+}
